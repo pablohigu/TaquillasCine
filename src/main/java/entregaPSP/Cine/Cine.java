7802fd4c40b4796e9a614384909fe7ae403cce6e
pablohigu <pablo.hi.do@gmail.com>
2025-12-30 13:41:20 +0100

    feat: Parametros y funciones clase cine

--- src/main/java/entregaPSP/Cine/Cine.java
+++ src/main/java/entregaPSP/Cine/Cine.java
@@ -1,5 +1,66 @@
 package entregaPSP.Cine;
 
-public class Cine {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cine {
+    private int asientosDisponibles;
+    private List<Cliente> listaClientes; 
+    private boolean abierto;
+    private int entradasVendidas;
+
+    public Cine(int numeroAsientos) {
+        this.asientosDisponibles = numeroAsientos;
+        this.listaClientes = new ArrayList<>(); 
+        this.abierto = true;
+        this.entradasVendidas = 0;
+    }
+    public synchronized void entrarCine(Cliente cliente) {
+        if (abierto) {
+            listaClientes.add(cliente);
+            System.out.println("LOG: Cliente " + cliente.getId() + " entra a la lista de espera.");
+            notifyAll(); 
+        }
+    }
+
+    public synchronized void cerrarCine() {
+        this.abierto = false;
+        notifyAll(); 
+        System.out.println("--- CINE CERRADO ---");
+    }
+    public synchronized Cliente venderEntrada() {
+        while (listaClientes.isEmpty() && abierto) {
+            try {
+                wait(); 
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return null;
+            }
+        }
+        if (asientosDisponibles <= 0) {
+            return null; 
+        }
+
+        if (listaClientes.isEmpty()) {
+            return null;
+        }
+        Cliente clienteAtendido = listaClientes.remove(0); 
+        
+        asientosDisponibles--;
+        entradasVendidas++;
+        
+        return clienteAtendido;
+    }
+
+    public int getEntradasVendidas() {
+        return entradasVendidas;
+    }
+    
+    public int getGenteQuedaSinEntrada() {
+        return listaClientes.size(); 
+    }
+    
+    public boolean hayAsientosLibres() {
+        return asientosDisponibles > 0;
+    }
 }
--- src/main/java/entregaPSP/Cine/Cliente.java
+++ src/main/java/entregaPSP/Cine/Cliente.java
@@ -1,5 +1,11 @@
 package entregaPSP.Cine;
 
 public class Cliente {
-
+private String id;
+public Cliente(String id) {
+	this.id = id;
+}
+public String getId() {
+	return id;
+}
 }
