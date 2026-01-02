04c776a3635bf5ee8f71f04434c90a6a5c5121fd
pablohigu <pablo.hi.do@gmail.com>
2026-01-02 19:54:26 +0100

    feat: Clase Cine Modificada para colas

--- src/main/java/entregaPSP/Cine/Cine.java
+++ src/main/java/entregaPSP/Cine/Cine.java
@@ -5,62 +5,87 @@ import java.util.List;
 
 public class Cine {
     private int asientosDisponibles;
-    private List<Cliente> listaClientes; 
+    private List<List<Cliente>> sistemaColas; 
+    
+    private int maxPorCola;
+    private int numeroColas;
+    
     private boolean abierto;
     private int entradasVendidas;
 
-    public Cine(int numeroAsientos) {
+   
+    public Cine(int numeroAsientos, int numeroColas, int maxPorCola) {
         this.asientosDisponibles = numeroAsientos;
-        this.listaClientes = new ArrayList<>(); 
+        this.numeroColas = numeroColas;
+        this.maxPorCola = maxPorCola;
         this.abierto = true;
         this.entradasVendidas = 0;
-    }
-    public synchronized void entrarCine(Cliente cliente) {
-        if (abierto) {
-            listaClientes.add(cliente);
-            System.out.println("LOG: Cliente " + cliente.getId() + " entra a la lista de espera.");
-            notifyAll(); 
+        this.sistemaColas = new ArrayList<>();
+        for (int i = 0; i < numeroColas; i++) {
+            sistemaColas.add(new ArrayList<>());
         }
     }
-
-    public synchronized void cerrarCine() {
-        this.abierto = false;
-        notifyAll(); 
-        System.out.println("--- CINE CERRADO ---");
-    }
-    public synchronized Cliente venderEntrada() {
-        while (listaClientes.isEmpty() && abierto) {
-            try {
-                wait(); 
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                return null;
+    public synchronized boolean entrarCine(Cliente cliente) {
+        if (!abierto) return false;
+        
+       
+        for (int i = 0; i < numeroColas; i++) {
+            List<Cliente> cola = sistemaColas.get(i);
+            
+            if (cola.size() < maxPorCola) {
+                cola.add(cliente);
+                System.out.println("LOG: Cliente " + cliente.getId() + " entra a la COLA " + (i+1) 
+                        + " (" + cola.size() + "/" + maxPorCola + ")");
+                notifyAll(); 
+                return true; 
             }
         }
-        if (asientosDisponibles <= 0) {
+        
+       
+        System.out.println("❌ Cliente " + cliente.getId() + " se marcha: TODAS LAS COLAS LLENAS.");
+        return false;
+    }
+
+    public synchronized Cliente despacharCliente(int indiceCola) {
+        
+   
+        if (asientosDisponibles <= 0 || indiceCola < 0 || indiceCola >= numeroColas) {
+            return null;
+        }
+
+        List<Cliente> colaSeleccionada = sistemaColas.get(indiceCola);
+        
+        if (colaSeleccionada.isEmpty()) {
             return null; 
         }
 
-        if (listaClientes.isEmpty()) {
-            return null;
-        }
-        Cliente clienteAtendido = listaClientes.remove(0); 
+     
+        Cliente cliente = colaSeleccionada.remove(0);
         
         asientosDisponibles--;
         entradasVendidas++;
         
-        return clienteAtendido;
+   
+        notifyAll(); 
+        
+        return cliente;
     }
 
-    public int getEntradasVendidas() {
-        return entradasVendidas;
+    public synchronized void cerrarCine() {
+        this.abierto = false;
+        notifyAll();
+        System.out.println("--- CINE CERRADO ---");
     }
     
-    public int getGenteQuedaSinEntrada() {
-        return listaClientes.size(); 
-    }
-    
-    public boolean hayAsientosLibres() {
-        return asientosDisponibles > 0;
+    public boolean hayAsientosLibres() { return asientosDisponibles > 0; }
+    public boolean estaAbierto() { return abierto; }
+    public int getEntradasVendidas() { return entradasVendidas; }
+    public int getNumeroColas() { return numeroColas; }
+   
+    public synchronized boolean hayClientesEsperando() {
+        for(List<Cliente> c : sistemaColas) {
+            if(!c.isEmpty()) return true;
+        }
+        return false;
     }
 }
